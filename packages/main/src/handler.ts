77cba5386905e6e8e0b8fac8eb4f0cb3e245781f
Sean Behan <codebam@riseup.net>
2023-07-09 18:43:48 -0400

    fix the webhook url log message

--- packages/main/src/handler.ts
+++ packages/main/src/handler.ts
@@ -1,78 +1,111 @@
 import BotApi from "./bot_api";
 import { sha256, log } from "./libs";
-import { Config, PartialConfig, Update, localhost } from "./types";
+import {
+  Webhook,
+  Commands,
+  Config,
+  PartialConfig,
+  Update,
+  localhost,
+} from "./types";
 
 export default class Handler {
-	configs: PartialConfig[];
+  configs: PartialConfig[];
 
-	constructor(configs: PartialConfig[]) {
-		this.configs = configs;
+  constructor(configs: PartialConfig[]) {
+	this.configs = configs;
+  }
+
+  getResponse = async (
+	_request?: Request,
+	_bot?: BotApi
+  ): Promise<Response> => {
+	this.getAccessKeys(this.configs).then((access_keys) =>
+	  Object.keys(access_keys).forEach((key) =>
+		log(
+		  `${access_keys[key].bot_name} ${((request_url) =>
+			`${request_url.origin}${request_url.pathname}`)(
+			new URL(_request?.url ?? localhost)
+		  )}/${key}`
+		)
+	  )
+	);
+	if (_bot?.webhook.token) {
+	  return _bot.webhook.process(new URL(_request?.url ?? localhost));
 	}
+	return this.responses.default();
+  };
 
-	getResponse = async (
-		_request?: Request,
-		_bot?: BotApi
-	): Promise<Response> => {
-		this.getAccessKeys(this.configs).then((access_keys) =>
-			Object.keys(access_keys).forEach((key) =>
-				log(
-					`${access_keys[key].bot_name} ${((request_url) =>
-						`${request_url.origin}${request_url.pathname}`)(
-						new URL(_request?.url ?? localhost)
-					)}/${key}`
-				)
-			)
-		);
-		if (_bot?.webhook.token) {
-			return _bot.webhook.process(new URL(_request?.url ?? localhost));
-		}
-		return this.responses.default();
-	};
+  postResponse = async (_request?: Request, _bot?: BotApi): Promise<Response> =>
+	_bot?.webhook.token === ""
+	  ? this.responses.default()
+	  : _request
+	  ? _request
+		  .json()
+		  .then((update) => (_bot as BotApi).update(update as Update))
+	  : this.responses.default();
 
-	postResponse = async (_request?: Request, _bot?: BotApi): Promise<Response> =>
-		_bot?.webhook.token === ""
-			? this.responses.default()
-			: _request
-			? _request
-					.json()
-					.then((update) => (_bot as BotApi).update(update as Update))
-			: this.responses.default();
+  responses: Record<
+	string,
+	(_request?: Request, _bot?: BotApi) => Promise<Response>
+  > = {
+	GET: this.getResponse,
+	POST: this.postResponse,
+	default: () => new Promise(() => new Response()),
+  };
 
-	responses: Record<
-		string,
-		(_request?: Request, _bot?: BotApi) => Promise<Response>
-	> = {
-		GET: this.getResponse,
-		POST: this.postResponse,
-		default: () => new Promise(() => new Response()),
-	};
+  getAccessKeys = async (
+	configs: PartialConfig[]
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ): Promise<Record<string, any> | Record<string, never>> =>
+	Promise.all(
+	  configs.map((bot_config: PartialConfig) =>
+		sha256(bot_config.webhook?.token ?? "").then((hash) => [
+		  hash,
+		  bot_config,
+		])
+	  )
+	).then((result) => Object.fromEntries(result));
 
-	getAccessKeys = async (
-		configs: PartialConfig[]
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	): Promise<Record<string, any> | Record<string, never>> =>
-		Promise.all(
-			configs.map((bot_config: PartialConfig) =>
-				sha256(bot_config.webhook?.token ?? "").then((hash) => [
-					hash,
-					bot_config,
-				])
-			)
-		).then((result) => Object.fromEntries(result));
+  // check
+  check_undefined = (x: any) => {
+	if (x === undefined) {
+	  return {};
+	}
+	return x;
+  };
 
-	// handles the request
-	handle = async (request: Request): Promise<Response> =>
-		this.getAccessKeys(this.configs).then((access_keys) =>
-			Object.keys(this.responses).includes(request.method)
-				? this.responses[request.method](
-						request,
-						new access_keys[new URL(request.url).pathname.substring(1)].api({
-							...new Config(),
-							url: new URL(new URL(request.url).origin), // worker url
-							handler: this,
-							...access_keys[new URL(request.url).pathname.substring(1)],
-						})
-				  )
-				: this.responses.default()
-		);
+  // handles the request
+  handle = async (request: Request): Promise<Response> =>
+	this.getAccessKeys(this.configs).then((access_keys) =>
+	  Object.keys(this.responses).includes(request.method)
+		? this.responses[request.method](
+			request,
+			((access_keys) => {
+			  if (
+				access_keys[new URL(request.url).pathname.substring(1)] ===
+				undefined
+			  ) {
+				return () => {
+				  return new BotApi(
+					{},
+					new Webhook(
+					  new URL("https://seanbehan.ca"),
+					  "",
+					  new URL("https://seanbehan.ca")
+					),
+					new Handler([])
+				  );
+				};
+			  }
+			  return access_keys[new URL(request.url).pathname.substring(1)];
+			})(access_keys)({
+			  ...new Config(),
+			  url: new URL(new URL(request.url).origin), // worker url
+			  handler: this,
+			  ...access_keys[new URL(request.url).pathname.substring(1)],
+			})
+		  )
+		: this.responses.default()
+	);
 }
